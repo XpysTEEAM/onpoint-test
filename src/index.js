ad25ac1207dd1c7942153bc5fce0a3d24dc888e0
Jenya <nikolaev-evgenij@bk.ru>
2022-06-04 00:38:33 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './Components/app/App';
+
+ReactDOM
+   .createRoot(document.getElementById('root'))
+   .render(
+      <App />
+   )
